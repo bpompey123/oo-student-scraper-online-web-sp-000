9eb34181212f8a5503522ab6438b3a6f5aacfe23
Brian Pompey <brianthescholar@gmail.com>
2019-11-01 17:35:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,7 +23,7 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     hash = {}
     doc = Nokogiri::HTML(open(profile_url))
-      hash[:bio] = doc.css("div.description-holder").text
+      hash[:bio] = doc.css(".description-holder a").text
       hash[:profile_quote] = doc.css(".profile-quote").text
 
     links = doc.css(".social-icon-container").css("a")
