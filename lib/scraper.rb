7bbea94865789a91dae5880f833dcd906c1fe103
Brian Pompey <brianthescholar@gmail.com>
2019-11-01 17:22:57 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,8 +12,10 @@ class Scraper
         student_name = student.css(".student-name").text
         student_location = student.css(".student-location").text
         student_url = student.attr("href")
-      end
+
       students << {name: student_name, location: student_location, profile_url: student_url}
+      end
+
     end
     students
   end
