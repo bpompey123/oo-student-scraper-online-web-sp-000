f6f9b399578e1148937a916c0931cd58fd0a9893
Brian Pompey <brianthescholar@gmail.com>
2019-11-01 17:16:38 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,8 @@ class Scraper
 
     index_page.css("div.roster-cards-container").each do |card|
       card.css(".student-card a").each do |student|
-  #      student_name = 
+  #      student_name =
+        binding.pry
       end
     end
   end
